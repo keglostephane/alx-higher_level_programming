381a03877d38b4f25f55f1e664df274910b62d13
Stephane Keglo <stephanekeglo@gmail.com>
2022-12-02 19:56:22 -0800

    add 0-print_list_integer.py script

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Print all integers of a list
+
+    Parameters:
+    my_list (list) : list of integers
+
+    Returns:
+    None
+    """
+    for num in my_list:
+        print("{}".format(num))
