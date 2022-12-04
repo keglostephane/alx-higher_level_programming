36fc3e07891d99158572163564dd74e08e43480e
Stephane Keglo <stephanekeglo@gmail.com>
2022-12-03 19:37:32 -0800

    fix 1-element_at.py script

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -10,6 +10,11 @@ def element_at(my_list, idx):
     my_list[idx] : element of `my_list` at index `idx`.
 
     None: if `idx` is negative or out of range.
+
+    >>> my_list = [1, 2, 3, 4, 5]
+    >>> idx = 3
+    >>> print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    Element at index 3 is 4
     """
     if idx < 0 or idx >= len(my_list):
         return None
