40e3a7c8691581f4892479adfa224002122bb2a2
Stephane Keglo <stephanekeglo@gmail.com>
2022-12-03 19:31:35 -0800

    fix 0-print_list_integer.py script

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    """Print all integers of a list
+    """Print all integers of a list.
 
-    Parameters:
+    Args:
     my_list (list) : list of integers
 
     Returns:
     None
 
-    Example:
     >>> my_list = [1, 2, 3, 4, 5]
     >>> print_list_integer(my_list)
+    1
+    2
+    3
+    4
+    5
     """
     for num in my_list:
         print("{:d}".format(num))
