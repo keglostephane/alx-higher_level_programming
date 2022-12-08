91a9046865007530741eb8a71e633c6d7e50e040
Stephane Keglo <stephanekeglo@gmail.com>
2022-12-08 14:41:09 -0800

    fix 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -20,7 +20,7 @@ def square_matrix_simple(matrix=[]):
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
     if matrix == []:
-        return [];
+        return []
     else:
         new_matrix = [[num ** 2 for num in row] for row in matrix]
         return new_matrix
