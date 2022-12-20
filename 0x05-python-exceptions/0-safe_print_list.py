78c491016034f237caca7bd782ccbe9ccd28a889
Stephane Keglo <stephanekeglo@gmail.com>
2022-12-19 16:33:18 -0800

    fix 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,7 +16,7 @@ def safe_print_list(my_list=[], x=0):
             for i in range(x):
                 print(f"{my_list[i]}", end='')
             print("")
-            return i + 1
+            return x
         except IndexError:
             print("")
             return i
