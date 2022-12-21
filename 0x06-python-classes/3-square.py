d9ae2bf5332dd7b1fef61e4e93f78957aca3beb9
Stephane Keglo <stephanekeglo@gmail.com>
2022-12-21 14:23:09 -0800

    add 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""A square module
+
+This module provides encapsulation to work with a square.
+"""
+
+
+class Square:
+    """Defines a square.
+
+    Attributes:
+        size (int) : size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Square object initializer.
+
+        Args:
+                size (int, default=0) : size of the square.
+
+        Returns:
+                (Square) : a `Square` object.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Computes the square area.
+
+        Returns:
+                (int) :  area of the square
+        """
+        return self.__size ** 2
