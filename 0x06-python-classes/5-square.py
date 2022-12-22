ea0bbb60b6564a6eca0f27f8fe8bd329a0234f4c
Stephane Keglo <stephanekeglo@gmail.com>
2022-12-21 16:06:22 -0800

    add 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""A square module
+
+This module provides encapsulation to work with a square.
+"""
+
+
+class Square:
+    """Defines a square.
+
+    Attributes:
+        size (int) : size of the square.
+    """
+
+    def __init__(self, size=0):
+        """Square object initializer.
+
+        Args:
+                size (int, default=0) : size of the square.
+
+        Returns:
+                (Square) : a `Square` object.
+        """
+        self.__size = size
+
+    def area(self):
+        """Computes the square area.
+
+        Returns:
+                (int) :  area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Gets `size` attribute value.
+
+        Returns:
+                (int) : `size` value
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets `size` attribute value.
+
+        Args:
+                value (int) : `size` attribute value
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """Prints the square with the character `#`"""
+        if self.__size == 0:
+            print("")
+        else:
+            side = self.__size
+            for i in range(side):
+                for j in range(side):
+                    print("#", end='')
+                print("")
