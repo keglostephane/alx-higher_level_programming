f81ca12dfd130baf1c52910bb2c94d18903b256e
Stephane Keglo <stephanekeglo@gmail.com>
2023-01-05 16:26:55 -0800

    add 0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A Rectangle Module
+
+This module provides Classe and methods to work with ``Rectangle``.
+"""
+
+
+class Rectangle:
+    """ Represents a ``Rectangle``. """
+    pass
