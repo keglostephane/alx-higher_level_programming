b8897ee289b6d10dd9b878048bd6289c1b8d8514
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-07 23:02:44 +0000

    Create 8-multiple_returns.py file

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin:python3
+def multiple_returns(sentence):
+    if sentence is not None:
+        if not sentence:
+            return 0, None
+        else:
+            return len(sentence), sentence[0]
