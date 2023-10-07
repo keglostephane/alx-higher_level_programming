daf76693f4aacf7aec38c551c38cec55e75e6e0f
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-07 23:20:54 +0000

    Fix shebang line

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
-#!/usr/bin:python3
+#!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is not None:
         if not sentence:
             return 0, None
         else:
-            length = len(sentence)
-            return length, sentence[0]
+            return len(sentence), sentence[0]
