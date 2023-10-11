f872f78c25e9848b89b777816b0c9390f4c8bba2
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-11 17:18:55 +0000

    Create 3-common_elements.py file

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """Returns a set of common elements in two sets.
+
+    :param set_1: first set
+    :type set_1: set
+    :param set_2: second set
+    :type set_2: set
+    :return: a set of common elements in the two sets.
+    :rtype: set
+    """
+    if set_1 and set_2 is not None:
+        return set_1.intersection(set_2)
