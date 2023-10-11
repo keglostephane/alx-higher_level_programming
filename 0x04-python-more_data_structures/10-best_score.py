7b81ff62fdfef2bc28c78032937bef663c37c656
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-11 21:07:37 +0000

    Fix 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -10,4 +10,8 @@ def best_score(a_dictionary):
     """
     if a_dictionary is None:
         return None
-    return max(a_dictionary)
+    best = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value == best:
+            return key
+    return None
