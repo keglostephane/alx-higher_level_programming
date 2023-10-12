a1467b286bdd864232b5cf384be1eb2157c0341c
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-12 01:32:27 +0000

    Create 100-weight_average.py file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """Return the weighted average of all integers tuple.
+
+    :param my_list: a list of tuples, defaults to []
+    :type my_list: list, optional
+    :return: the weighted average of all integers tuple.
+        0 if the list is empty
+    :rtype: int, float
+    """
+    sum = 0
+    weight = 0
+
+    for a, b in my_list:
+        sum += a * b
+        weight += b
+    return sum / weight
