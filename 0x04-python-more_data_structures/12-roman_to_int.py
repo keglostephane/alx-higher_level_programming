a2c6365e12f1f198af4bb8d45e5e23849d8fe09a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-14 01:59:35 +0000

    Create 12-roman_to_int.py file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from functools import reduce
+
+
+def roman_to_int(roman_string):
+    """Convert a Roman numeral to an integer.
+
+    :param roman_string: a Roman numeral
+    :type roman_string: str
+    :return: an integer value of the Roman numeral.
+        If the roman_string is not a string or None, return 0
+    :rtype: int
+    """
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                 'M': 1000}
+
+    result = 0
+    prev = 0
+    for ch in roman_string:
+        value = roman_map[ch]
+
+        if prev < value:
+            if not prev:
+                result = value
+            else:
+                result -= prev
+                result += value - prev
+        else:
+            result += value
+
+        prev = value
+    return result
