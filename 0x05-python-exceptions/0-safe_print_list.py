cab24ee2673455d175a224178b365b588bed0b0d
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-23 22:17:30 +0000

    Improve the code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,9 +10,6 @@ def safe_print_list(my_list=[], x=0):
     :return: the number of elements printed
     :rtype: int
     """
-    if x <= 0:
-        print("")
-        return 0
     try:
         p = 0
         for i in range(x):
