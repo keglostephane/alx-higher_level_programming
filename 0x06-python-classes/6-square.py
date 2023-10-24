a9df07795946e0ea17009c032eb412521745d9c4
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-24 21:36:07 +0000

    Fix code output #3

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -78,6 +78,6 @@ class Square:
         else:
             for i in range(self.__position[1]):
                 print("")
-            for i in range(self.__size):
+            for j in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
