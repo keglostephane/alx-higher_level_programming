43c79b90f47b1fc389f5a224b9ebd80e976c5592
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-24 00:04:59 +0000

    Fix code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -13,6 +13,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as error:
-        sys.stderr.write("Exception: {}\n".format(error))
+    except (ValueError, TypeError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
