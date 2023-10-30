b41aece85440cb5966efab599f325a7188144646
Dodzi Keglo <stephanekeglo@gmail.com>
2023-10-30 22:43:17 +0000

    Fix 1-rectangle.py #2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
         :param value: value of the rectangle
         :type value: int
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -60,9 +60,9 @@ class Rectangle:
         :param value: the height of the rectangle
         :type value: int
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must >= 0")
+            raise ValueError("height must be >= 0")
 
         self.__height = value
