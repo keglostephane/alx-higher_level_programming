e3a888c720de3f679bc150313c5f81c1c82f8d33
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-01 02:20:11 +0000

    Create 4-print_square.py file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""4-print_square
+
+This module provide ``print_square`` that prints a square with the character #
+"""
+
+def print_square(size):
+    """Print a square with the character #.
+
+    :param size: size of the square
+    :type size: int
+    :raises TypeError: size must be an integer
+    :raises ValueError: size must be >= 0
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
