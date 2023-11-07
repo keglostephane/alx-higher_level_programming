1c209702b7bd578320fe0d7f2785bc4a7007d25e
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-07 20:21:57 +0000

    Create 5-save_to_json_file.py script

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""5-save_to_json_file
+
+This module provide ``save_to_json_file`` that writes an object to a text file,
+using a JSON representation.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON representation.
+
+    :param my_obj: the object to serialize
+    :type my_obj: any
+    :param filename: name of the text file
+    :type filename: str
+    """
+    with open(filename, 'w', encoding='utf-8') as fp:
+        json.dump(my_obj, fp)
