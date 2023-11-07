6e951e12845b6de285b1712ec72778d3cd948c51
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-07 17:43:00 +0000

    Create 0-read_file.py script

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""0-read_file
+
+This module provide ``read_file`` that reads a text file and print it
+to stdout.
+"""
+
+
+def read_file(filename=""):
+    """Read a file and print its content to stdout.
+
+    :param filename: name of the file to print
+    :type filename: str
+    """
+    with open(filename, encoding="UTF8") as fp:
+        for line in fp.readlines():
+            print(line, end="")
