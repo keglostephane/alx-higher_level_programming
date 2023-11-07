bc7d3e111bf1a17f37c314f73a884036fbab3149
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-07 02:28:22 +0000

    Create 11-square.py script

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""11-square
+
+This module provide a ``Square`` class.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square.
+
+    :param size: size of the square
+    :type size: int
+    :raise TypeError: size must be an integer
+    :raise valueError: size must be greater than 0
+    """
+    def __init__(self, size):
+        """Initializer"""
+        self.__size = super().integer_validator("size", size)
+
+    def area(self):
+        """Compute the area of the square.
+
+        :return: the area of the square
+        :rtype: int
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """Human readable represantation of Square"""
+        return f"[Square] {self.__size}/{self.__size}"
