f660f772f543f5bd22a6d17f0603164136efa23a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-07 01:22:28 +0000

    Create 7-base_geometry.py script

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""5-base_geometry
+
+This module provide ``BaseGeometry`` class.
+"""
+
+
+class BaseGeometry:
+    """An Base Geometry class.
+    """
+    def area(self):
+        """Not yet implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate a value.
+
+        :param name: name of the value
+        :type name: str
+        :param value: value of ``name``
+        :type value: int
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
