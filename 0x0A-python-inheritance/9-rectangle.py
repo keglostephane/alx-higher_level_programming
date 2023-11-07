8a336d9a865dbd4d9bc63afe2853bad9359bd866
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-07 01:59:32 +0000

    Create 9-rectangle.py script

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""9-rectangle
+
+This module provide ``Rectangle`` class.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle.
+
+    :param width: width of thr rectangle
+    :type width: int
+    :raise TypeError: width must be an integer
+    :raise ValueError: width must be greater than 0
+    :param height: height of the rectangle
+    :type height: int
+    :raise TypeError: height must be an integer
+    :raise ValueError: height must be greater than 0
+    """
+    def __init__(self, width, height):
+        """Initializer"""
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
+    def area(self):
+        """Compute the area of the rectangle.
+
+        :return: the area of the rectangle
+        :rtype: int
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Human readable representation of Rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
