f623a279bb80fafb49674cb6e623380880ffd5b6
Dodzi Keglo <stephanekeglo@gmail.com>
2023-11-14 09:35:51 +0000

    Add size private attribute with validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,9 +7,11 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """A square class.
 
-    :param width: width of the Square
+    :param size: size of the square
+    :type size: int
+    :param width: width of the Square, same as size
     :type witdh: int
-    :param height: height of the Square
+    :param height: height of the Square, same as size
     :type height: int
     :param x: position on X-axis
     :type x: int
@@ -20,7 +22,33 @@ class Square(Rectangle):
     """
     def __init__(self, size, x=0, y=0, id=None):
         """Square initializer"""
+        self.size = size
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Return the size of the square
+
+        :return: the square's size
+        :rtype: int
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square
+
+        :param value: value of the square's size
+        :type value: int
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__size = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Human readable representation of Square"""
