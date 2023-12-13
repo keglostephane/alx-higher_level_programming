efaf6e5e7b9099edf4355bf676b099bed6536f6e
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-13 03:03:04 +0000

    Create 10-top_score.sql script

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of the table `second_table` of the database `hbtn_0c_0`
+-- Display both the score and the name
+-- Records should be ordered by score (top first)
+-- database name will be passed as an argument of mysql command
+SELECT score, name FROM second_table ORDER BY score DESC
