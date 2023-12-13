79591e2c169a3147bcd9055754d6dde85bbdda5d
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-13 19:18:42 +0000

    Create 2-create_read_user.sql script

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- Create the database `hbtn_0d_2` and the user `user_0d_2`
+-- `user_0d_2` should have SELECT privilege in the database `hbtn_0d_2`
+-- `user_0d_2` password should be set to user_0d_2_pwd
+-- if the database `hbtn_0d_2` already exists, script should not fail
+-- if the user `user_0d_2` already exists, script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER
+       IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
