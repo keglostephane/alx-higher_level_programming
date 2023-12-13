d2abb343b751df820be2c38969cca3f6d82433d3
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-13 03:17:12 +0000

    Create 14-average.sql script

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,7 @@
+-- Computes the score average of all records in the table `second_table` of
+-- database `hbtn_0c_0`
+
+-- result column name should be average
+-- database name will be passed as an argument of mysql command
+
+SELECT AVG(score) as average FROM second_table
