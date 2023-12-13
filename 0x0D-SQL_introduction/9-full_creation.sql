6060bc5be36b1ad3bc8be9fc6fb1337f4e089f08
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-13 02:59:50 +0000

    Add comments

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,20 @@
+-- Creates a table `second_table` in the database `hbtn_0c_0 and
+-- add multiple rows
+
+-- `second_table` description:
+-- id INT
+-- name VARCHAR(256)
+-- score INT
+
+-- database name is passed as an argument to mysql command
+-- if `second_table` already exists, don't fail
+
+-- create these records:
+-- id = 1, name = “John”, score = 10
+-- id = 2, name = “Alex”, score = 3
+-- id = 3, name = “Bob”, score = 14
+-- id = 4, name = “George”, score = 8
+
 CREATE TABLE IF NOT EXISTS second_table (
        id INT,
        name VARCHAR(256),
