d3758c9bd24f313831530cef4754eeb91eb8ef69
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-13 03:12:34 +0000

    Create 13-change_class.sql script

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,6 @@
+-- Removes all records with a score <= 5 in the table `second_table` of database
+-- `hbtn_0c_0`
+
+-- database name will be passed as an argument of mysql command
+
+DELETE FROM second_table WHERE score <= 5
