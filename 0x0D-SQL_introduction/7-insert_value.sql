1a776d80c84dd9b4950d6250923dc5dcc72b9878
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-13 02:42:07 +0000

    Create 7-insert_value.sql script

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Inserts a new row in the table `first_table` in database `hbtn_0c_0
+-- database name is passed as an argument of mysql command
+INSERT INTO first_table (id, name) VALUES (89, 'Best School')
