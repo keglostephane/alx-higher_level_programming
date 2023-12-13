bcb771f66dd91a251f57a4a81708718f6bd38026
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-13 03:08:47 +0000

    Create 12-no_cheating.sql script

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- Update the score of Bob to 10 in the table `second_table`.
+
+-- update the score of Bob using only the name field.
+-- database name will passed as an argument of the mysql command
+
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
