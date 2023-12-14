f3c03cfb3af619a88c5dbbf82d13f4dcdae73779
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-14 02:04:53 +0000

    Create 10-genre_id_by_show.sql script

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,12 @@
+-- List all shows contained in `hbtn_0d_tvshows` that have
+-- at least one genre linked
+
+-- Each record displays `tv_shows.title` - `tv_show_genres.genre_id`
+-- Results are sorted in ascending order by `tv_shows.title` and
+-- `tv_show_genres.genre_id
+-- database name will be passed as argument to mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+       FROM tv_shows INNER JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+       ORDER BY tv_shows.title, tv_show_genres.genre_id;
