5e571d5281a7e9a9cf1ca8730d94d2c141da2202
Dodzi Keglo <stephanekeglo@gmail.com>
2023-12-15 03:06:55 +0000

    Fix script #1

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -12,4 +12,5 @@ SELECT name FROM tv_genres
 	       	      ON tv_shows.id = tv_show_genres.show_id
 	       	      INNER JOIN tv_genres
 	       	      ON tv_show_genres.genre_id = tv_genres.id
-	       	      WHERE tv_shows.title = 'Dexter' );
+	       	      WHERE tv_shows.title = 'Dexter')
+	ORDER BY name ASC;
