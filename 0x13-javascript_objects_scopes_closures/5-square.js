cb53f1e4b7f108eb1946c0e50ab21f70533ef4ad
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-09 23:57:51 +0000

    Add 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let str = '';
+    for (let i = 0; i < this.width; i++) {
+      str += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
