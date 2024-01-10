d44d17baa08c4c29916757f9ee06199c401b00a5
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-10 00:29:04 +0000

    Fix 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,40 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Square1 = require('./5-square');
 
-  print () {
-    let str = '';
-    for (let i = 0; i < this.width; i++) {
-      str += 'X';
-    }
-    for (let j = 0; j < this.height; j++) {
-      console.log(str);
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Square1 {
   charPrint (c) {
     if (c) {
       let str = '';
