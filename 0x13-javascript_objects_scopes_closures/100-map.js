5fbab7e58132b70c1674a946b2e25042baaceed6
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-10 02:22:56 +0000

    Fix 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const newList = list.map((num) => num * list.indexOf(num));
+let index = 0;
+const newList = list.map((num) => num * index++);
 
 console.log(list);
 console.log(newList);
