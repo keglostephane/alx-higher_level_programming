06229077bf1be2c4611eefd55b2ee7010695039c
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-14 16:05:07 +0000

    Add 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""safe_filter_states_by_user_input
+
+Filter states from `states` tables by user input from database given as
+parameter
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3],
+        port=3306)
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s"
+    param = (sys.argv[4],)
+    cursor.execute(query, param)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
