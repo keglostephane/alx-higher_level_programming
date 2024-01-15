b63c863d3dfe290005597e938ecce6374c295050
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-15 17:18:44 +0000

    Add relationship_city.py, relationship_state.py, 100-relationship_states_cities.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Link class City to State
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
