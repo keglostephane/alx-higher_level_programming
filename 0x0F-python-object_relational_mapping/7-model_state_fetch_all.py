eddc37231d2e7524e32ff5734ccddd22b42a70f1
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-15 01:51:48 +0000

    Don't execute if imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,14 +5,14 @@
 List all State objects from the database given as parameter.
 """
 import sys
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-with Session(engine) as session:
-    query = select(State.id, State.name)
-    for state in session.execute(query).all():
-        print(f"{state[0]}: {state[1]}")
+    with Session(engine) as session:
+        for state in session.query(State.id, State.name).all():
+            print(f"{state[0]}: {state[1]}")
