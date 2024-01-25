ada4266f7a919b7858e03ff66e4d1e5c6f4a5a4e
Dodzi Keglo <stephanekeglo@gmail.com>
2024-01-25 21:37:26 +0000

    Fix #1

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server accept
-curl -sIX OPTIONS 0.0.0.0:5000/route_4 | grep "Allow" | grep -Po "(?<=Allow: ).*" | cat
+curl -sIX OPTIONS "$1" | grep -Po "(?<=Allow: ).*" | cat
