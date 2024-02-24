bd8df6beac41363fcc8333d14506b02c9c15b9d8
Dodzi Keglo <stephanekeglo@gmail.com>
2024-02-24 15:00:56 +0000

    Fix code logic

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,19 +6,23 @@ const args = process.argv;
 const request = require('request');
 
 if (args.length === 3) {
-  const filmId = args[2];
+  const filmId = parseInt(args[2], 10);
   const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
-  const url = 'https://swapi-api.alx-tools.com/api/people';
 
-  request.get(url, (error, response, body) => {
+  request.get(filmUrl, (error, response, body) => {
     if (error) {
       console.log(error);
     } else {
-      const people = JSON.parse(body).results;
-      for (const person of people) {
-        for (const film of person.films) {
-          if (film === filmUrl) console.log(person.name);
-        }
+      const film = JSON.parse(body);
+      for (const url of film.characters) {
+        request.get(url, (error, response, body) => {
+          if (error) {
+            console.log(error);
+          } else {
+            const person = JSON.parse(body);
+            console.log(person.name);
+          }
+        });
       }
     }
   });
