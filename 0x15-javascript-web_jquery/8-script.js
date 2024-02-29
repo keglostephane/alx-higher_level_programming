a20d49a3ea8804154ef6a81be2082aa161082ef9
Dodzi Keglo <stephanekeglo@gmail.com>
2024-02-29 04:17:57 +0000

    Add 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+const url = 'https://swapi-api.alx-tools.com/api/films/?format=json';
+$.get(url, function (data) {
+  $.each(data.results, function (index, film) {
+    $('ul#list_movies').append('<li>' + film.title + '</li>');
+  });
+});
